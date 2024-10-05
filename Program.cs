c6db8072172b3c02f67e38e01b2a431f286abb5d
Adriancas28 <castroadrian1228@gmail.com>
2024-10-04 19:18:22 -0500

    Creando la Base de Datos

--- Program.cs
+++ Program.cs
@@ -2,12 +2,14 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PARCIAL_CASTRO.Data;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("PosgreConnection") 
+    ?? throw new InvalidOperationException("Connection string 'PosgreConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseNpgsql(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
